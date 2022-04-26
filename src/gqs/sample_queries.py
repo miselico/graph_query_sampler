998a4711917247498f21f6b570e9912029d3aa3c
Michael Cochez <miselico@users.noreply.github.com>
2022-04-26 18:54:45 +0200

    Added check that the constraint is applied on IRIs only. Fixing #20

--- src/gqs/sample_queries.py
+++ src/gqs/sample_queries.py
@@ -66,6 +66,7 @@ def preprocess_formulas(dataset: Dataset, sparql_endpoint: str, sparql_endpoint_
                     graph <split:all> {
                         ?s ?p ?entity .
                         }
+                        isIRI(?entity)
                     } group by ?entity
                     having (count(?entity) > ''' + str(restriction["maximum"]) + ' )'
             elif restriction["type"] == "max_outdegree":
@@ -74,6 +75,7 @@ def preprocess_formulas(dataset: Dataset, sparql_endpoint: str, sparql_endpoint_
                     graph <split:all> {
                         ?entity ?p ?o .
                         }
+                        isIRI(?entity)
                     } group by ?entity
                     having (count(?entity) > ''' + str(restriction["maximum"]) + ' )'
             else:
